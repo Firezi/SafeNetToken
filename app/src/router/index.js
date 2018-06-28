b9461c65c7c7da9db0b9016cbdcd1d174af532de
nvalidov <niyazvalidov@gmail.com>
2018-06-28 10:20:30 +0300

    ;

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import SntRequest from '@/components/snt-request'
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
