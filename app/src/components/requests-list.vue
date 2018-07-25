c98c16c9988f1f5e73db469f204c8108eb452ce3
nvalidov <niyazvalidov@gmail.com>
2018-07-25 16:05:10 +0300

    requests List

--- app/src/components/requests-list.vue
+++ app/src/components/requests-list.vue
@@ -1,17 +1,38 @@
 <template>
-  <h1>Requests List</h1>
+  <div>
+    <h1>Requests List</h1>
+    <ul>
+      <li v-for="request in $store.state.requestsList">
+        <div>
+          <h3>Request #{{ request.id }}</h3>
+          <h4>{{ types[request.rType] }}</h4>
+          <p>{{ request.beneficiary }}</p>
+          <p>{{ (request.tokensAmount / 10**18).toFixed(2) }} SNT</p>
+          <p v-if="request.rType == 2"> {{ (request.ethAmount / 10**18).toFixed(3) }} Eth</p>
+          <p v-if="request.rType < 2">Treaty: {{ request.treatyHash }}</p>
+          <p>{{ request.isConfirmed == 1 ? 'Confirmed' : 'Not confirmed' }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "requests-list",
+  data: function () {
+    return {
+      types: [
+        'Owner',
+        'Team',
+        'Eth investor',
+        'Fiat investor '
+      ],
+    }
+  },
   beforeCreate() {
     console.log('getRequestsList Action dispatched');
     this.$store.dispatch('getRequestsList');
   },
 }
 </script>
-
-<style scoped>
-
-</style>
--- app/src/util/getRequestsList.js
+++ app/src/util/getRequestsList.js
@@ -3,12 +3,12 @@ import getContract from './getContract'
 
 let getRequestsList = new Promise(function(resolve, reject) {
   getContract.then(result => {
-    result.methods.requestsCount().call().then((len) => {
+    result.methods.requestsCount().call().then(len => {
       let promisesList = [];
       for (let i = 0; i < len; i++) {
         promisesList[i] = result.methods.requests(i).call();
       }
-      Promise.all(promisesList).then((requestsList) => {
+      Promise.all(promisesList).then(requestsList => {
         resolve(requestsList);
       })
     })
