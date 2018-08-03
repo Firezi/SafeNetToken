ef87c061de765ef9da345477731a591225c0d941
nvalidov <niyazvalidov@gmail.com>
2018-08-04 00:35:04 +0300

    main page

---
+++ app/src/components/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <router-link :to="{ name: 'enter-request' }">Create Request</router-link>
+    <br>
+    <router-link :to="{ name: 'requests-list' }">Requests List</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: "main"
+  }
+</script>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -3,12 +3,18 @@ import Router from 'vue-router'
 import EnterRequest from '@/components/enter-request'
 import RequestsList from '@/components/requests-list'
 import Request from  '@/components/request'
+import Main from '@/components/main'
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main
+    },
     {
       path: '/list',
       name: 'requests-list',
