a2437d2753179bd306b079bf5cc5a6a9f33031bf
nvalidov <niyazvalidov@gmail.com>
2018-09-10 17:46:25 +0300

    requests list

--- app/src/components/requests-list.vue
+++ app/src/components/requests-list.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <h1>Requests List</h1>
-    <ul>
-      <li v-for="request in $store.state.requestsList">
-        <div>
-          <router-link :to="{ name: 'request', params: {requestId: request.id} }">Request #{{ request.id }}</router-link>
-          <p>{{ types[request.rType] }}</p>
-          <p>{{ request.beneficiary }}</p>
-          <p v-if="request.rType < 4">{{ (request.tokensAmount / 10**18).toFixed(2) }} SNT</p>
-          <p v-if="request.rType == 4">{{ request.percentage }}%</p>
-          <p v-if="request.rType == 2"> {{ (request.ethAmount / 10**18).toFixed(3) }} Eth</p>
-          <p>{{ status[request.isConfirmed] }}</p>
-        </div>
-      </li>
-    </ul>
+    <div v-for="request in $store.state.requestsList">
+      <b-card class="mt-3" header-bg-variant="light" :header="types[request.rType]">
+        <b-row class="mb-1">
+          <b-col cols="auto" class="mr-auto">Request №{{ request.id }}</b-col>
+          <b-col cols="auto">{{ request.beneficiary }}</b-col>
+        </b-row>
+        <div class="mb-1" v-if="request.rType < 4">{{ (request.tokensAmount / 10**18).toFixed(2) }} SNT</div>
+        <div class="mb-1" v-if="request.rType == 4">{{ request.percentage }}%</div>
+        <div class="mb-1" v-if="request.rType == 2"> {{ (request.ethAmount / 10**18).toFixed(3) }} Eth</div>
+        <b-row>
+          <b-col cols="auto" class="mr-auto">
+            <b-button :to="{ name: 'request', params: {requestId: request.id} }" variant="primary">Go to request</b-button>
+          </b-col>
+          <b-col cols="auto">
+            <h5><b-badge :variant="badge[request.isConfirmed]">{{ status[request.isConfirmed] }}</b-badge></h5>
+          </b-col>
+        </b-row>
+
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -33,6 +40,11 @@ export default {
         'Not confirmed',
         'Declined by Investor',
         'Confirmed'
+      ],
+      badge: [
+        'warning',
+        'danger',
+        'success'
       ]
     }
   },
