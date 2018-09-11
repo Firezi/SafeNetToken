695f99d5942e4071fa5c134918607dca9012b72e
nvalidov <niyazvalidov@gmail.com>
2018-09-11 11:05:02 +0300

    list in profile

--- app/src/components/user-page.vue
+++ app/src/components/user-page.vue
@@ -5,18 +5,37 @@
     <p>Group: {{ groups[$store.state.user.group] }}.</p>
     <p>Balance: {{ getBalance }} SNT.</p>
     <h2>Your requests:</h2>
-    <ul>
-      <li v-for="request in $store.state.requestsList">
-        <div>
-          <router-link :to="{ name: 'request', params: {requestId: request.id} }">Request #{{ request.id }}</router-link>
-          <p>{{ types[request.rType] }}</p>
-          <p>{{ request.beneficiary }}</p>
-          <p>{{ (request.tokensAmount / 10**18).toFixed(2) }} SNT</p>
-          <p v-if="request.rType == 2"> {{ (request.ethAmount / 10**18).toFixed(3) }} Eth</p>
-          <p>{{ status[request.isConfirmed] }}</p>
-        </div>
-      </li>
-    </ul>
+    <div v-for="request in $store.state.requestsList">
+      <b-card class="mb-3" header-bg-variant="light" :header="types[request.rType]">
+        <b-row class="mb-1">
+          <b-col cols="auto" class="mr-auto">Request №{{ request.id }}</b-col>
+          <b-col cols="auto">{{ request.beneficiary }}</b-col>
+        </b-row>
+        <b-row class="mb-1" v-if="request.rType < 4">
+          <b-col cols="1" class="deskr">Tokens:</b-col>
+          <b-col>{{ (request.tokensAmount / 10**18).toFixed(2) }} SNT</b-col>
+        </b-row>
+        <b-row class="mb-1" v-if="request.rType == 4">
+          <b-col cols="1" class="deskr">Percentage:</b-col>
+          <b-col>{{ request.percentage }}%</b-col>
+        </b-row>
+        <b-row class="mb-1" v-if="request.rType == 2">
+          <b-col cols="1" class="deskr">Eth amount:</b-col>
+          <b-col>{{ (request.ethAmount / 10**18).toFixed(3) }} Eth</b-col>
+        </b-row>
+        <!--<div class="mb-1" v-if="request.rType == 4">{{ request.percentage }}%</div>-->
+        <!--<div class="mb-1" v-if="request.rType == 2"> {{ (request.ethAmount / 10**18).toFixed(3) }} Eth</div>-->
+        <b-row>
+          <b-col cols="auto" class="mr-auto">
+            <b-button :to="{ name: 'request', params: {requestId: request.id} }" variant="primary">Go to request</b-button>
+          </b-col>
+          <b-col cols="auto">
+            <h5><b-badge :variant="badge[request.isConfirmed]">{{ status[request.isConfirmed] }}</b-badge></h5>
+          </b-col>
+        </b-row>
+
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -51,6 +70,11 @@
           'Not confirmed',
           'Declined by Investor',
           'Confirmed'
+        ],
+        badge: [
+          'warning',
+          'danger',
+          'success'
         ]
       }
     },
@@ -62,3 +86,8 @@
   }
 </script>
 
+<style>
+  .deskr {
+    color: gray;
+  }
+</style>
